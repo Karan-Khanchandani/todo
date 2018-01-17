55a03f8a1ffb559e9de7dff7911ae7e82fe123d9
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-01-17 23:41:03 +0530

    Adds Vue to display posts

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Posts from '@/components/Posts'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }, {
+    path: '/posts',
+    name: 'Posts',
+    component: Posts
+  }]
 })
